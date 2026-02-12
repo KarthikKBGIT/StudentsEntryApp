365dff9269917fb411ce65ecb6693e01045e764b
karthikkb <karthikkb1012@gmail.com>
2026-02-12 23:07:00 +0530

    Added default redirect page post login

--- src/main/java/com/karthik/StudentEntryApp/config/SecurityConfiguration.java
+++ src/main/java/com/karthik/StudentEntryApp/config/SecurityConfiguration.java
@@ -25,9 +25,9 @@ public class SecurityConfiguration {
                 .authorizeHttpRequests(auth ->
                         auth.requestMatchers(HttpMethod.POST, "/register").permitAll().
                                 anyRequest().authenticated())
-                .formLogin(Customizer.withDefaults())
+                .formLogin(form -> form.permitAll().defaultSuccessUrl("/", true))
                 .httpBasic(Customizer.withDefaults())
-                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED))
+                //.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED))
                 .build();
     }
 
